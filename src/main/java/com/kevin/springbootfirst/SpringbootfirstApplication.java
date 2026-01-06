43457d284c27404ebe4f4d81d7b6084d825df272
ya.nematov <nematovmuhammadqodir68@gmail.com>
2026-01-06 23:57:15 +0900

    DI

--- src/main/java/com/kevin/springbootfirst/SpringbootfirstApplication.java
+++ src/main/java/com/kevin/springbootfirst/SpringbootfirstApplication.java
@@ -2,13 +2,17 @@ package com.kevin.springbootfirst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbootfirstApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootfirstApplication.class, args);
-		System.out.println("Hello world");
+		ApplicationContext context = SpringApplication.run(SpringbootfirstApplication.class, args);
+
+		Alien alien1 = context.getBean(Alien.class);
+		alien1.code();
+
 	}
 
 }
