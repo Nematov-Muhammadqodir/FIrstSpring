a585898e6382bfefbe486672cb29325ea4163dee
ya.nematov <nematovmuhammadqodir68@gmail.com>
2026-01-06 23:47:31 +0900

    feat: Initailize Spring Boot

---
+++ src/main/java/com/kevin/springbootfirst/SpringbootfirstApplication.java
@@ -0,0 +1,14 @@
+package com.kevin.springbootfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootfirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootfirstApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
