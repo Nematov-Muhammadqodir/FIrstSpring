181102da1f294cea558e1a45627307a289799921
ya.nematov <nematovmuhammadqodir68@gmail.com>
2026-01-07 00:24:21 +0900

    @Autowired

--- src/main/java/com/kevin/springbootfirst/Alien.java
+++ src/main/java/com/kevin/springbootfirst/Alien.java
@@ -1,10 +1,15 @@
 package com.kevin.springbootfirst;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Alien {
+
+    @Autowired
+    Laptop laptop;
+
     public void code() {
-        System.out.println("Kevin is learning Java");
+        this.laptop.compile();
     }
 }
---
+++ src/main/java/com/kevin/springbootfirst/Laptop.java
@@ -0,0 +1,10 @@
+package com.kevin.springbootfirst;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+    public void compile() {
+        System.out.println("Compiling...");
+    }
+}
